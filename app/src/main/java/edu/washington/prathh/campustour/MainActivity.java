33a6b78377a83dc5a42976c05dc2035f06193bfc
Hillary Prather <prathh@uw.edu>
2015-03-03 15:01:14 -0800

    Camera mode shows, pointers don't

--- app/src/main/java/edu/washington/prathh/campustour/MainActivity.java
+++ app/src/main/java/edu/washington/prathh/campustour/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends ActionBarActivity {
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, GeoCameraActivity.class);
+                Intent intent = new Intent(MainActivity.this, GeoCamContent.class);
                 startActivity(intent);
             }
         });
